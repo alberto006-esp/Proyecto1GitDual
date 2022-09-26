787823a458fa967928fad23be7f652d0ca172bc8
alberto006-esp <64962627+alberto006-esp@users.noreply.github.com>
2022-09-26 13:13:51 +0200

    Update App.java

--- src/main/java/com/everis/mvnASO/App.java
+++ src/main/java/com/everis/mvnASO/App.java
@@ -11,7 +11,7 @@ public class App
     public static void main( String[] args )
     {
     	if(StringUtils.isBlank(" ")) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!      " );
     	}else {
     		System.out.println("Bye");
     	}
